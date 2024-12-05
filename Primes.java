ce08278a21f0d52e7992737d9b7791e7af031932
lallyshayah <lally.shayah@post.runi.ac.il>
2024-12-05 21:54:30 +0200

    Final Solution

--- Primes.java
+++ Primes.java
@@ -1,53 +1,33 @@
 public class Primes {
     public static void main(String[] args) {
-        int n = Integer.parseInt(args[0]);
-        boolean[] arrP = new boolean[n+1];
-
-        arrP[0] = false;
-        arrP[1] = false;
         
-        for (int i = 2; i <= n; i++)
-        {
-            arrP[i] = true;
-        }
-
-        int p = 2;
-       
-
-        for (int i = p; i  < arrP.length; i++)
-        {
-            boolean firstTime = true;
-
-            if (i % p == 0 && firstTime == true)
-               {
-                arrP[p] = true;
-                firstTime = false;
-               }
-
-            if ( i < arrP.length - 1)
-            {
-                for (int j = i + 1; j < arrP.length; j++)
-            {
-               if (j % p == 0 && firstTime == false)
-               {
-                arrP[j] = false;
-               }
-            }
-
-            if (arrP[i + 1] == true)
-            {
-                p = i + 1;
-            }
-
-            }
-        } 
-
-        for (int i = 0; i > arrP.length; i++)
-        {
-            if (arrP[i] == true)
-            {
+        System.out.println( "Prime numbers up to " + (args [0] + ":"));
+        int n = Integer.parseInt(args[0]);
+        int count = 0;
+        int i = 2;
+        while (i <= n) {
+            
+            if (i == 2) {
                 System.out.println(i);
+                count ++;
+            } 
+            
+            else {
+                int num = i -1;
+
+                while (i % num != 0) {
+                    num --;
+
+                    if (num == 1) {
+                        count ++;
+                        System.out.println(i);
+                        break;
+                    }
+                }
             }
+            i++;
         }
+        
+        System.out.println("There are " + count + " primes between 2 and " + n + " (" + ((count * 100) / n) + "% are primes)");
    }
 }
